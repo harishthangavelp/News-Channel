758b38e4703ba87c78a41e696ab5440cb1d678e8
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-12-04 20:33:03 +0530

    news-channel

--- News-Project/my-news-channel/src/User/Register.jsx
+++ News-Project/my-news-channel/src/User/Register.jsx
@@ -38,7 +38,7 @@ const Register = () => {
     
         try {
             // Send data to backend
-            const response = await fetch("http://localhost:5000/addUser", {
+            const response = await fetch("https://news-channel-14.onrender.com/addUser", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
